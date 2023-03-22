bc506bdd5f350675f0e499a3900927675e935901
ingvega <isc.p.vega@gmail.com>
2023-03-22 15:59:21 -0600

    Ejercicio con tablas

--- unidad3/tablas/js/scriptCalif.js
+++ unidad3/tablas/js/scriptCalif.js
@@ -1,6 +1,24 @@
 $(document).ready(()=>{
     $("#calificaciones").DataTable({
-        responsive: true,
+        responsive: {
+            details: {
+                renderer: function ( api, rowIdx, columns ) {
+                    var data = $.map( columns, function ( col, i ) {
+                        return col.hidden ?
+                            '<tr data-dt-row="'+col.rowIndex+'" data-dt-column="'+col.columnIndex+'">'+
+                                '<td>'+col.title+':'+'</td> '+
+                                '<td>'+col.data+'</td>'+
+                            '</tr>' :
+                            '';
+                    } ).join('');
+     
+                    return data ?
+                        $('<table/>').append( data ) :
+                        false;
+                }
+                //display: $.fn.dataTable.Responsive.display.modal()
+            }
+        },
         columnDefs:[
             {responsivePriority:2,targets:0},
             {responsivePriority:1,targets:[1,2,3,4,5,6,7,8,9,10,11]}
