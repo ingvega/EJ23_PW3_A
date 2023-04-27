a6fa2e22cfb5458229de72c43b86cdf9ad7f8535
ingvega <isc.p.vega@gmail.com>
2023-04-27 11:58:14 -0600

    Animaciones con JS

--- unidad4/texto/funciones.js
+++ unidad4/texto/funciones.js
@@ -1,23 +1,38 @@
+var animaciones=[];
+function disminuir(){
+animaciones.forEach((animacion)=>{
+    console.log(animacion);
+    debugger;
+});
+
+}
 document.addEventListener("DOMContentLoaded",()=>{
+
     //Definir los keyframes
     let rotar=[
-        { transform: 'rotateX(90deg)' },
+        { transform: 'rotateX(80deg)' },
         { transform: 'rotateX(0deg)' }
     ];
 
+    let duracion=100;
     //Configurar la animación
     let config={
-        duration:1000
+       // iterations:Infinity,
+        delay:0,
+        duration:1000,
+        fill:'forwards'
     };
-
-    //console.log(document.querySelector(".texto div"));
     
+    //console.log(document.querySelector(".texto div"));
     let letras=document.querySelectorAll(".texto div");
     console.log(letras);
     letras.forEach((e,i)=>{
+    
         console.log(e);
         console.log(i);
-        e.animate(rotar,config);
+        config.iterations = Infinity;
+        config.delay=duracion*i;
+        animaciones.push(e.animate(rotar,config));
     });
 
 });
